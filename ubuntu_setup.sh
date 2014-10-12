33252077f0579f9eccfe97241a7f5947bc5e5033
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2014-10-12 22:35:16 +0100

    ---Added new ubuntu setup bash script. Installs most needed packages, python, rvm, ruby, CR.

---
+++ ubuntu_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Installing system packages..."
+sudo apt-get install vim git gcc gnuplot gnuplot-x11 vlc gparted subversion curl gsl-bin libgsl0-dev
+
+echo "Making vim backup directory..."
+mkdir .vim_backup
+
+echo "Setting up python virtual environment and installing basic packages..."
+sudo apt-get install build-essential libatlas-dev libatlas3gf-base python-dev libjpeg-dev libxml2-dev libfreetype6-dev libpng-dev liblapack-dev
+sudo apt-get install python-pip
+sudo pip install virtualenv
+sudo virtualenv python_virt_env
+. ~/python_virt_env/bin/activate
+sudo pip install numpy scipy matplotlib scikit-learn
+deactivate
+
+echo "Installig packages needed for GS2..."
+sudo apt-get install gcc gfortran openmpi-bin libfftw3-3 libfftw3-mpi-dev libfftw3-dev libfftw3-bin libnetcdf-dev netcdf-bin
+
+echo "Downloading and installing CR and gs2crmod..."
+sudo curl -sSL https://get.rvm.io | bash -s stable
+. ~/.rvm/scripts/rvm
+type rvm | head -n 1
+rvm install 2.1.2
+rvm use 2.1 --default
+
+mkdir code
+cd code
+svn co svn://svn.code.sf.net/p/coderunner/svn/installer_new coderunner_svn
+cd coderunner_svn
+mkdir $HOME/custom_software
+sudo make -j 4 hd all rb PREFIX=$HOME/custom_software
+sudo apt-get install rubygems-integration ruby-dev
+sudo gem install rb-gsl coderunner gs2crmod genecrmod
+
+
+
+
+
+
+
