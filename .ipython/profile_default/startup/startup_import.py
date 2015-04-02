52133b07f8bea79ec983f3aa1d10619477b62707
ferdinandvwyk <ferdinandvwyk@gmail.com>
2015-04-02 09:51:58 +0100

    update ipython packages

--- .ipython/profile_default/startup/startup_import.py
+++ .ipython/profile_default/startup/startup_import.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
+from scipy.io import netcdf
+import scipy.interpolate as interp
 import seaborn as sns
 import pandas as pd
 sns.set_context('talk')
