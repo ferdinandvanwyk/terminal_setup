aac58935aa42c2d6360ba328bcc97b9808b1e7fb
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-06-10 00:16:22 +0100

    update ipython defaults

--- .ipython/profile_default/startup/startup_import.py
+++ .ipython/profile_default/startup/startup_import.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 from scipy.io import netcdf
 import scipy.interpolate as interp
 import seaborn as sns
 import pandas as pd
+
 sns.set_context('talk')
+sns.set_style('whitegrid')
+mpl.rcParams['font.family'] = 'serif'
 
